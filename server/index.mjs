315456dd72edc278ea37896a4eae82fb4934376f
sandesh-06 <sandeshsaravanan456@gmail.com>
2023-11-25 23:46:57 +0530

    changes done

--- server/index.mjs
+++ server/index.mjs
@@ -1,8 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from "dotenv"; //to access env variables
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT 
 
+// MONGOOSE SETUP
+const connectToMongo = async()=>{
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("Database connected!")
+}
+connectToMongo().catch((error)=>{console.log("Error connecting to database")})
+
+//SERVER LISTEN
 app.listen(PORT, ()=>console.log(`Server Port: ${PORT}`))
