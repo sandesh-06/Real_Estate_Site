46e01be6e6e4d344479c1edd6bd099cc568b6e25
sandesh-06 <sandeshsaravanan456@gmail.com>
2023-11-25 23:13:38 +0530

    server set up done

---
+++ server/index.mjs
@@ -0,0 +1,8 @@
+import express from 'express';
+import dotenv from "dotenv"; //to access env variables
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT 
+
+app.listen(PORT, ()=>console.log(`Server Port: ${PORT}`))
